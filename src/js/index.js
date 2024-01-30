8b43a12bd302c0356c95c25c8240487066aa890e
abraham <abrahamluccas13@gmail.com>
2024-01-30 20:58:54 -0300

    drama bg working

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,60 @@
 const drop = document.getElementById('drop').children;
 
-const winter = document.getElementById('winter');
+const era = {
+        drama:{
+                id: drop[0]
+        },
+        better:{
+                id: drop[1]
+        },
+        girls:{
+                id: drop[2]
+        },
+        mamba:{
+                id: drop[3]
+        }
+}
 
-console.log(winter);
+const girls = {
+        home:{
+                id: document.getElementById('home')
+        },
+        karina:{
+                id: document.getElementById('karina')
+        },
+        winter:{
+                id: document.getElementById('winter')
+        },
+        giselle:{
+                id: document.getElementById('giselle')
+        },
+        ning:{
+                id: document.getElementById('ning')
+        }
+}
+
+
+console.log(drop);
 
 //const path = '/assets/img/';
 
-drop[0].addEventListener('click', () =>{
-        winter.style.backgroundImage = 'url(/assets/img/drama/winterd2.jpg)';
+era.drama.id.addEventListener('click', () =>{
+        girls.home.id.style.backgroundImage = 'url(/assets/img/drama/aespad2.jpg)';
+        girls.winter.id.style.backgroundImage = 'url(/assets/img/drama/winterd2.jpg)';
+        girls.karina.id.style.backgroundImage = 'url(/assets/img/drama/karinaD.jpg)';
+        girls.giselle.id.style.backgroundImage = 'url(/assets/img/drama/giselled2.jpg)';
+        girls.ning.id.style.backgroundImage =  'url(/assets/img/drama/ningD.jpg)';
+
+})
+
+era.better.id.addEventListener('click', () =>{
+
+})
+
+era.girls.id.addEventListener('click', ()=>{
+
+})
+
+era.mamba.id.addEventListener('click', ()=>{
+
 })
