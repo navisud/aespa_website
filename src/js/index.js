b96c47ef85fc9fd9c28be54d246431369a44b234
abraham <abrahamluccas13@gmail.com>
2024-01-31 17:52:48 -0300

    all done

--- src/js/index.js
+++ src/js/index.js
@@ -7,11 +7,14 @@ const era = {
         better:{
                 id: drop[1]
         },
-        girls:{
+        savage:{
                 id: drop[2]
         },
-        mamba:{
+        world:{
                 id: drop[3]
+        },
+        life:{
+                id: drop[4]
         }
 }
 
@@ -34,31 +37,43 @@ const girls = {
 }
 
 
-console.log(drop);
-
-//const path = '/assets/img/';
-
 era.drama.id.addEventListener('click', () =>{
-        girls.home.id.style.backgroundImage = 'url(/assets/img/drama/aespad2.jpg)';
-        girls.winter.id.style.backgroundImage = 'url(/assets/img/drama/winterd2.jpg)';
-        girls.karina.id.style.backgroundImage = 'url(/assets/img/drama/karinaD.jpg)';
-        girls.giselle.id.style.backgroundImage = 'url(/assets/img/drama/giselled2.jpg)';
-        girls.ning.id.style.backgroundImage =  'url(/assets/img/drama/ningD.jpg)';
+        girls.home.id.style.backgroundImage = 'url(./src/assets/img/drama/aespad2.jpg)';
+        girls.winter.id.style.backgroundImage = 'url(./src/assets/img/drama/winterd2.jpg)';
+        girls.karina.id.style.backgroundImage = 'url(./src/assets/img/drama/karinaD.jpg)';
+        girls.giselle.id.style.backgroundImage = 'url(./src/assets/img/drama/giselled2.jpg)';
+        girls.ning.id.style.backgroundImage =  'url(./src/assets/img/drama/ningd2.jpg)';
 
-})
+});
 
 era.better.id.addEventListener('click', () =>{
-        girls.home.id.style.backgroundImage = 'url(/assets/img/better/homeB.jpg)';
-        girls.winter.id.style.backgroundImage = 'url(/assets/img/better/winterB.jpg)';
-        girls.karina.id.style.backgroundImage = 'url(/assets/img/better/karinaB.jpg)';
-        girls.giselle.id.style.backgroundImage = 'url(/assets/img/better/giselleB.jpg)';
-        girls.ning.id.style.backgroundImage =  'url(/assets/img/better/ningB.jpg)';
-})
+        girls.home.id.style.backgroundImage = 'url(./src/assets/img/better/homeB.jpg)';
+        girls.winter.id.style.backgroundImage = 'url(./src/assets/img/better/winterB.jpg)';
+        girls.karina.id.style.backgroundImage = 'url(./src/assets/img/better/karinaB.jpg)';
+        girls.giselle.id.style.backgroundImage = 'url(./src/assets/img/better/giselleB.jpg)';
+        girls.ning.id.style.backgroundImage =  'url(./src/assets/img/better/ningB.jpg)';
+});
 
-era.girls.id.addEventListener('click', ()=>{
+era.savage.id.addEventListener('click', ()=>{
+        girls.home.id.style.backgroundImage = 'url(./src/assets/img/savage/homeS3.jpg)';
+        girls.winter.id.style.backgroundImage = 'url(./src/assets/img/savage/winterS3.jpg)';
+        girls.karina.id.style.backgroundImage = 'url(./src/assets/img/savage/karinaS3.jpg)';
+        girls.giselle.id.style.backgroundImage = 'url(./src/assets/img/savage/giselleS3.jpg)';
+        girls.ning.id.style.backgroundImage =  'url(./src/assets/img/savage/ningS3.jpg)';
+});
 
-})
+era.world.id.addEventListener('click', ()=>{
+        girls.home.id.style.backgroundImage = 'url(./src/assets/img/world/homeM.jpg)';
+        girls.winter.id.style.backgroundImage = 'url(./src/assets/img/world/winterM3.jpg)';
+        girls.karina.id.style.backgroundImage = 'url(./src/assets/img/world/karinaM2.jpg)';
+        girls.giselle.id.style.backgroundImage = 'url(./src/assets/img/world/giselleM2.jpg)';
+        girls.ning.id.style.backgroundImage =  'url(./src/assets/img/world/ningM4.jpg)';
+});
 
-era.mamba.id.addEventListener('click', ()=>{
-
-})
+era.life.id.addEventListener('click', ()=>{
+        girls.home.id.style.backgroundImage = 'url(./src/assets/img/life/homeL1.jpg)';
+        girls.winter.id.style.backgroundImage = 'url(./src/assets/img/life/winterL2.jpg)';
+        girls.karina.id.style.backgroundImage = 'url(./src/assets/img/life/karinaL.jpg)';
+        girls.giselle.id.style.backgroundImage = 'url(./src/assets/img/life/giselleL2.jpg)';
+        girls.ning.id.style.backgroundImage =  'url(./src/assets/img/life/ningL2.jpg)';
+});
